b3b9de430614ed0a6a7f004ca0356e7cd8763cc9
Ruth James <jamesruth449@gmail.com>
2023-05-28 19:55:12 +0100

    header completer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div id="app" class="w-[1000px]">
+  <div id="app" class="lg:min-w-[1000px] md:min-w-[768px] min-w-[640px] box-border font-PlusJakartaSans">
     <section>
       <AppHeader />
+      <div class="bg-black text-white md:text-red lg:text-green">
+        desktop
+      </div>
+      <div class="bg-black text-white md:text-red lg:text-green">mobile</div>
+      <div class="bg-black text-white md:text-red lg:text-green">tablet</div>
     </section>
     
   </div>
